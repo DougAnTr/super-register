ebafb6adf3d1be3866656040c1b04605bb3bb039
Douglas Trofino <douglas.trofino@outlook.com>
2021-04-22 08:28:43 -0300

    Config typeorm

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import UserRoutes from "./routes/user.routes";
+
+class App {
+  private server: express.Application;
+  private port: string | number;
+
+  constructor() {
+    this.server = express();
+    this.config().then(() => {
+      this.routes();
+    });
+  }
+
+  public init() {
+    this.server.listen(this.port, () => {
+      console.log(`Application running on http://localhost:${this.port}`);
+    });
+  }
+
+  private async config() {
+    await createConnection();
+    this.port = process.env.PORT || 4000;
+    this.server.use(express.json);
+  }
+
+  private routes() {
+    this.server.use("users", UserRoutes);
+  }
+}
+
+const app = new App();
+app.init();
--- src/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import UsersController from "./app/users/users.controller";
-
-const routes = Router();
-
-routes.post("/users", new UsersController().store);
-
-export default routes;
